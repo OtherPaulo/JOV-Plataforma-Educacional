a2d865b221b3c29236e001bd94ad4fb21a54bd23
Paulo Rodrigues <paulovinicius356@gmail.com>
2024-10-03 17:40:22 -0300

    att video

--- client/src/components/home-view/hero.jsx
+++ client/src/components/home-view/hero.jsx
@@ -5,25 +5,24 @@ import React from "react";
 const HeroSection = () => {
   return (
     <div className="relative h-[660px] w-full overflow-hidden">
-      {/* Video Background */}
       <video
         autoPlay
         loop
         muted
-        className="absolute top-0 left-0 min-h-full min-w-full object-cover"
+        playsInline
+        className="absolute top-0 left-0 w-full h-full object-cover"
       >
-        <source src="/background-video.mp4" type="video/mp4" />
+        <source
+          src="https://klirva.nyc3.cdn.digitaloceanspaces.com/bg.mp4"
+          type="video/mp4"
+        />
         Your browser does not support the video tag.
       </video>
 
-      {/* Overlay */}
       <div className="absolute inset-0 bg-black bg-opacity-50" />
 
-      {/* Content */}
       <div className="relative z-10 flex flex-col items-center justify-center h-full text-white">
-        <button className="bg-orange-500 rounded-full p-6 mb-8 hover:bg-orange-600 transition-colors duration-300">
-          <Play className="w-12 h-12" />
-        </button>
+
         <h1 className="text-4xl md:text-5xl lg:text-4xl font-bold text-center mb-8 max-w-4xl">
           A maior plataforma de capacitação TECH
         </h1>
