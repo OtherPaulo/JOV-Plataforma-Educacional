3d8a9dcaf4897bd477637b4a3be70fc14820807a
elnatacorrea <elnatacorrea@hotmail.com>
2024-10-04 00:17:37 -0400

    pagina files do admin-view

--- client/src/pages/admin-view/files.jsx
+++ client/src/pages/admin-view/files.jsx
@@ -1,5 +1,4 @@
-import { Fragment } from "react";
-import React, { useState } from "react";
+import { useState, Fragment } from "react";
 import { Button } from "@/components/ui/button";
 import {
   Sheet,
@@ -9,7 +8,7 @@ import {
 } from "@/components/ui/sheet";
 import CommonForm from "@/components/common/form";
 import { addProductFormElements } from "@/config";
-import ProductFolterUpload from "@/components/admin-view/folter.upload";
+import ProductFolderUpload from "@/components/admin-view/folder.upload";
 
 const InitialFormData = {
   image: null,
@@ -26,35 +25,33 @@ function AdminFiles() {
   const [openCreateProductsDialog, setOpenCreateProductsDialog] =
     useState(false);
   const [formData, setFormData] = useState(InitialFormData);
-  const [folterFile, setFolterFile] = useState(null);
-  const [uploadedFolterUrl, setUploadedFolterUrl] = useState("");
+  const [folderFile, setFolderFile] = useState(null);
+  const [uploadedFolderUrl, setUploadedFolderUrl] = useState("");
 
   function onSubmit() {}
 
   return (
     <Fragment>
       <div className="mb-5 w-full flex justify-end">
-        <Button onClick={() => setOpenCreateProductsDialog(true)}>
+        <Button aria-label="Adicionar novo arquivo" onClick={() => setOpenCreateProductsDialog(true)}>
           Novo Arquivo
         </Button>
       </div>
-      <div className="grid gap-4 md:grid-cols-3 lg: grid-cols-4"></div>
+      <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4"></div>
 
       <Sheet
         open={openCreateProductsDialog}
-        onOpenChange={() => {
-          setOpenCreateProductsDialog(false);
-        }}
+        onOpenChange={(open) => setOpenCreateProductsDialog(open)}
       >
         <SheetContent side="right" className="overflow-auto">
           <SheetHeader>
             <SheetTitle>Adicionar Novo Arquivo</SheetTitle>
           </SheetHeader>
-          <ProductFolterUpload
-            file={folterFile}
-            setFile={setFolterFile}
-            uploadedFolterUrl={uploadedFolterUrl}
-            setUploadedFolterUrl={setUploadedFolterUrl}
+          <ProductFolderUpload
+            folderFile={folderFile} 
+            setFolderFile={setFolderFile}
+            uploadedFolderUrl={uploadedFolderUrl}
+            setUploadedFolderUrl={setUploadedFolderUrl}
           />
           <div className="py-6">
             <CommonForm
